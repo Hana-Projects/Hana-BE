c1e67c30b6b8cc209c850a9d0a96493939e102ee
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-08 14:52:41 +0900

    project init

---
+++ src/main/java/com/hanabridge/api/DovotedApplication.java
@@ -0,0 +1,13 @@
+package com.hanabridge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DovotedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DovotedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanabridge/api/DovotedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanabridge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DovotedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
