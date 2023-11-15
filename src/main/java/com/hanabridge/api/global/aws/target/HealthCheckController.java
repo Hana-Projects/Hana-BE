5ab26eeb36c3d2c117ee83b1da268fb280a324a8
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-11-16 00:56:21 +0900

    feat: aws health check api 생성

--- src/main/java/com/hanabridge/api/global/aws/target/HealthCheckController.java
+++ src/main/java/com/hanabridge/api/global/aws/target/HealthCheckController.java
@@ -1,13 +1,15 @@
 package com.hanabridge.api.global.aws.target;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Health Check", description = "AWS ELB health check")
 public class HealthCheckController {
 
     @GetMapping("/health")
-    public Long healthcheck(){
+    public Long healthcheck() {
         return 200L;
     }
 }
