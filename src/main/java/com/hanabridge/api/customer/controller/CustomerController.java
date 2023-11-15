a13164edcef7fb257c5e71d6ee0363457ce74dad
Lee sang min <dkwkghkdlxld2@gmail.com>
2023-11-16 00:56:43 +0900

    feat: customer controller 생성

--- src/main/java/com/hanabridge/api/customer/controller/CustomerController.java
+++ src/main/java/com/hanabridge/api/customer/controller/CustomerController.java
@@ -1,7 +1,25 @@
 package com.hanabridge.api.customer.controller;
 
+
+import com.hanabridge.api.customer.dto.CustomerRegisterRequest;
+import com.hanabridge.api.global.dto.ApiSuccessResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "유저 API", description = "유저 관련 API")
+@RequestMapping("/api")
 @RestController
+@RequiredArgsConstructor
 public class CustomerController {
+
+
+//    @PostMapping("/register")
+//    public ResponseEntity<ApiSuccessResponse<Void>> register(@RequestBody CustomerRegisterRequest request){
+//
+//    }
 }
