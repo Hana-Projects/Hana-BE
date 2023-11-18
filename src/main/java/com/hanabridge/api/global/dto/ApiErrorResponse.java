b4aba5733035b70fedb62c8e7123cd9599ebfd7b
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-18 16:46:49 +0900

    feat: ApiErrorResponse 메소드 생성

--- src/main/java/com/hanabridge/api/global/dto/ApiErrorResponse.java
+++ src/main/java/com/hanabridge/api/global/dto/ApiErrorResponse.java
@@ -1,7 +1,10 @@
 package com.hanabridge.api.global.dto;
 
+import com.hanabridge.api.global.code.ErrorCode;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
 
 
 @Getter
@@ -10,4 +13,13 @@ public class ApiErrorResponse {
 
     private int code;
     private String message;
+
+    public static ResponseEntity<ApiErrorResponse> fromErrorCode(ErrorCode errorCode) {
+        return ResponseEntity.status(errorCode.getHttpStatus())
+                .body(
+                        ApiErrorResponse.builder()
+                                .code(errorCode.getHttpStatus().value())
+                                .message(errorCode.getMessage())
+                                .build());
+    }
 }
