41a82601c8a68d4be9d67112a92e4c21a00eb29d
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-18 16:35:19 +0900

    feat: ErrorCode 추가

--- src/main/java/com/hanabridge/api/global/code/ErrorCode.java
+++ src/main/java/com/hanabridge/api/global/code/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND,"존재하지 않습니다.");
+    USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 username의 고객이 존재하지 않습니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST,"일치하지 않는 비밀번호입니다.");
 
     private HttpStatus httpStatus;
     private String message;
