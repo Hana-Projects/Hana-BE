68c4058c8c0dd4dea0c47eedfc64aadc006c738a
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-18 16:36:11 +0900

    feat: 생성자 추가

--- src/main/java/com/hanabridge/api/global/dto/ApiSuccessResponse.java
+++ src/main/java/com/hanabridge/api/global/dto/ApiSuccessResponse.java
@@ -1,11 +1,13 @@
 package com.hanabridge.api.global.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class ApiSuccessResponse<T> {
 
     private int code;
