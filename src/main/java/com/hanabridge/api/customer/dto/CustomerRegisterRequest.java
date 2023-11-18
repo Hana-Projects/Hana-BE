a34e96cda28caf415b9d32de7eb7db88e025fc32
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-18 16:33:29 +0900

    docs: 회원 가입 명세서 model 정보 추가

--- src/main/java/com/hanabridge/api/customer/dto/CustomerRegisterRequest.java
+++ src/main/java/com/hanabridge/api/customer/dto/CustomerRegisterRequest.java
@@ -1,13 +1,19 @@
 package com.hanabridge.api.customer.dto;
 
 import com.hanabridge.api.customer.domain.Customer;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Schema(name = "회원 가입 요청 model")
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CustomerRegisterRequest {
+    @Schema(name = "유저 id", example = "haha123")
     String username;
+    @Schema(name = "패스워드", example = "123")
     String password;
 
     public Customer toEntity() {
