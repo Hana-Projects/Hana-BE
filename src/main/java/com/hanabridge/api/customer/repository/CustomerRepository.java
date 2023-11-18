d1e820416a77a93b27de26dc006e6967c8affb70
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-18 16:34:03 +0900

    feat: CustomerRepository spring data jpa 메소드 생성

--- src/main/java/com/hanabridge/api/customer/repository/CustomerRepository.java
+++ src/main/java/com/hanabridge/api/customer/repository/CustomerRepository.java
@@ -2,6 +2,13 @@ package com.hanabridge.api.customer.repository;
 
 import com.hanabridge.api.customer.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByUsername(String username);
+
 }
