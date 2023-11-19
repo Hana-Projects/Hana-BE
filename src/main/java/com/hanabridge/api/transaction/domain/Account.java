cba7e115b94d0d69a5101d06cd2614708b066e61
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-19 13:10:09 +0900

    feat: Account entity 생성

---
+++ src/main/java/com/hanabridge/api/transaction/domain/Account.java
@@ -0,0 +1,45 @@
+package com.hanabridge.api.transaction.domain;
+
+import com.hanabridge.api.customer.domain.Customer;
+import com.hanabridge.api.transaction.dto.AccountResponse;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "ACCOUNT")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ACCOUNT_NUMBER", nullable = false)
+    private String accountNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "CUSTOMER_ID", nullable = false)
+    private Customer customer;
+
+    @Column(name = "BANK_CODE")
+    @Enumerated(EnumType.STRING)
+    private BankCode bankCode;
+
+    @Column(name = "BALANCE")
+    private Long balance;
+
+    @Column(name = "ACCOUNT_CODE")
+    @Enumerated(EnumType.STRING)
+    private AccountCode accountCode;
+
+    public AccountResponse toAccountResponse() {
+        return AccountResponse.builder()
+            .accountCode(accountCode)
+            .accountNumber(accountNumber)
+            .balance(balance)
+            .build();
+    }
+}
