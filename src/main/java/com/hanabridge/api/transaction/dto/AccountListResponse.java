2f2bb88c1b2689fba031a5352ac95b335cb0905c
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-19 13:11:47 +0900

    feat: 계좌 목록 조회 응답 model 생성

---
+++ src/main/java/com/hanabridge/api/transaction/dto/AccountListResponse.java
@@ -0,0 +1,22 @@
+package com.hanabridge.api.transaction.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Schema(name = "계좌 목록 조회 model")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountListResponse {
+
+    @Schema(description = "총 계좌 갯수")
+    private int total;
+    @Schema(description = "소유한 계좌 각각의 정보(계좌 종류, 계좌번호, 잔액) 리스트")
+    private List<AccountResponse> accountResponses;
+}
