9b1b090d2aab80b0493007d1c2e646704c8249a7
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-19 13:12:37 +0900

    feat: 응답 성공 static 메소드 생성

--- src/main/java/com/hanabridge/api/global/dto/ApiSuccessResponse.java
+++ src/main/java/com/hanabridge/api/global/dto/ApiSuccessResponse.java
@@ -1,17 +1,29 @@
 package com.hanabridge.api.global.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.hanabridge.api.transaction.dto.AccountListResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Schema(name = "API 성공 응답 model")
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class ApiSuccessResponse<T> {
 
+    @Schema(description = "응답 코드")
     private int code;
+    @Schema(description = "응답에 대한 메세지")
     private String message;
+    @Schema(description = "응답 데이터")
     @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
     private T data;
+
+    public static <T> ApiSuccessResponse<T> success200(String message, T data) {
+        return new ApiSuccessResponse<>(201, message, data);
+    }
 }
