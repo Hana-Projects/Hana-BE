1c5625cb554b051664374c47b6c0d2ac86cbc032
leesangmin <dkwkghkdlxld2@gmail.com>
2023-11-26 12:36:22 +0900

    feat: APT 청약 응답 model 추가

---
+++ src/main/java/com/hanabridge/api/calendar/dto/APTDataResponse.java
@@ -0,0 +1,35 @@
+package com.hanabridge.api.calendar.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class APTDataResponse {
+
+    @JsonProperty("HOUSE_MANAGE_NO")
+    private Integer houseManageNo;
+    @JsonProperty("PBLANC_NO")
+    private Integer announcementNo;
+    @JsonProperty("HOUSE_NM")
+    private String houseName;
+    @JsonProperty("RCEPT_BGNDE")
+    private String receiptBeginDate;
+    @JsonProperty("RCEPT_ENDDE")
+    private String receiptEndDate;
+    @JsonProperty("HSSPLY_ADRES")
+    private String address;
+
+    public CalendarResponse toCalendarResponse() {
+        return CalendarResponse.builder()
+            .houseName(this.houseName)
+            .receiptEndDate(this.receiptEndDate)
+            .address(this.address)
+            .build();
+    }
+
+}
