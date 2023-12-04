0c6ce9c3ddaab11aa5838f07c1b15c62b7515e64
leesangmin <dkwkghkdlxld2@gmail.com>
2023-12-04 11:32:11 +0900

    feat: 상품 음성 안내 기능 추가

--- src/main/java/com/hanabridge/api/voice/service/VoiceService.java
+++ src/main/java/com/hanabridge/api/voice/service/VoiceService.java
@@ -3,6 +3,7 @@ package com.hanabridge.api.voice.service;
 import com.hanabridge.api.global.code.ErrorCode;
 import com.hanabridge.api.global.exception.DataNotFoundException;
 import com.hanabridge.api.transaction.domain.RemitCode;
+import com.hanabridge.api.voice.domain.ItemGuide;
 import com.hanabridge.api.voice.domain.RemitAccountGuide;
 import com.hanabridge.api.voice.domain.RemitCommonGuide;
 import com.hanabridge.api.voice.domain.RemitPhoneGuide;
@@ -11,6 +12,7 @@ import com.hanabridge.api.voice.dto.GuideRequest;
 import com.hanabridge.api.voice.dto.GuideResponse;
 import com.hanabridge.api.voice.dto.VoiceRequest;
 import com.hanabridge.api.voice.dto.VoiceResponse;
+import com.hanabridge.api.voice.repository.ItemGuideRepository;
 import com.hanabridge.api.voice.repository.RemitAccountGuideRepository;
 import com.hanabridge.api.voice.repository.RemitCommonGuideRepository;
 import com.hanabridge.api.voice.repository.RemitPhoneGuideRepository;
@@ -25,6 +27,7 @@ public class VoiceService {
     private final RemitCommonGuideRepository remitCommonGuideRepository;
     private final RemitAccountGuideRepository remitAccountGuideRepository;
     private final RemitPhoneGuideRepository remitPhoneGuideRepository;
+    private final ItemGuideRepository itemGuideRepository;
 
     @Transactional
     public VoiceResponse getInitGuide(VoiceRequest request) {
@@ -42,6 +45,7 @@ public class VoiceService {
         }
         throw new DataNotFoundException(ErrorCode.GUIDE_NOT_FOUND);
     }
+
     @Transactional
     public GuideResponse getGuide(GuideRequest request) {
 
@@ -63,17 +67,43 @@ public class VoiceService {
                 .map(RemitPhoneGuide::toGuideResponse)
                 .orElseThrow(() -> new DataNotFoundException(ErrorCode.GUIDE_NOT_FOUND));
 
+        } else if (isItem(request.getVoiceCode())) {
+            return itemGuideRepository.findById(request.getIndex())
+                .map(ItemGuide::toGuideResponse)
+                .orElseThrow(() -> new DataNotFoundException(ErrorCode.GUIDE_NOT_FOUND));
         }
-
         throw new DataNotFoundException(ErrorCode.GUIDE_NOT_FOUND);
+
     }
 
     private Boolean isRemit(String request) {
         return request.contains("송금") || request.contains("돈") || request.contains("입금")
-            || request.contains("보내");
+            || request.contains("보내") || request.contains("이체");
     }
 
     private Boolean isRemit(VoiceCode code) {
         return VoiceCode.REMIT == code;
+    }
+
+    private Boolean isItem(String request) {
+        return request.contains("상품") || request.contains("혜택") || request.contains("할인")
+            || request.contains("생활비") || request.contains("연금") || request.contains("증권")
+            || request.contains("집") || request.contains("가입") || request.contains("아이템")
+            || request.contains("해택") || request.contains("통장") || request.contains("주거래")
+            || request.contains("만들") || request.contains("카드") || request.contains("적금")
+            || request.contains("예금") || request.contains("계좌") || request.contains("금리")
+            || request.contains("펀드") || request.contains("저축") || request.contains("하나") ||
+            request.contains("추천") || request.contains("기름값") || request.contains("주유비")
+            || request.contains("장보기") || request.contains("마트") || request.contains("홈플러스")
+            || request.contains("학원") || request.contains("자격증") || request.contains("어학")
+            || request.contains("시험") || request.contains("통신비") || request.contains("요금")
+            || request.contains("핸드폰") || request.contains("휴대폰") || request.contains("스마트폰")
+            || request.contains("요금") || request.contains("숙박") || request.contains("여행")
+            || request.contains("버스비") || request.contains("지하철비") || request.contains("택시비")
+            || request.contains("병원") || request.contains("건강") || request.contains("의료");
+    }
+
+    private Boolean isItem(VoiceCode code) {
+        return VoiceCode.ITEM == code;
     }
 }
